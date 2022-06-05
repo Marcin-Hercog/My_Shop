990abdd9d7066ac504fae6ec5534695fc16bf511
Marcin Hercog <marcin.hercog.0@gmail.com>
2022-06-05 20:44:31 +0200

    Added Fluent API

--- MyShop/Context.cs
+++ MyShop/Context.cs
@@ -12,7 +12,7 @@ namespace MyShop
     class Context : DbContext
     {
         public DbSet<Address> Adresses { get; set; }
-        public DbSet<Category> Categories { get; set; }       
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
 
@@ -21,6 +21,33 @@ namespace MyShop
         {
             optionsBuilder.UseSqlServer("Server=(localdb)" +
                 "\\mssqllocaldb;Datebase=EFUDemyDB;Trusted_Connection=True;");
+        }
+
+        //using Fluent API here
+        //instead of Data Annotiation
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(eb =>
+            {
+                //With EntityTypeBuilder i can configurate properties of the Adress class
+                eb.Property(adress => adress.Cntry).IsRequired().HasColumnName("Country");
+                eb.Property(adress => adress.Street).IsRequired();
+                eb.Property(adress => adress.City).IsRequired();
+                eb.Property(adress => adress.PostalCode).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(eb =>
+            {
+                eb.Property(u => u.Name).IsRequired();
+                eb.Property(u => u.LastName).IsRequired();
+                eb.Property(u => u.Email).IsRequired();
+            });
+
+            modelBuilder.Entity<Product>(eb =>
+            {
+                eb.Property(p => p.Name).IsRequired();
+                eb.Property(p => p.Prize).IsRequired();
+            });
         }
     }
 }
