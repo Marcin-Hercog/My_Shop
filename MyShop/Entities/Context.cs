66c88d6240d7f50d4a1bd1a546c72c9ad0416cb2
Marcin Hercog <marcin.hercog.0@gmail.com>
2022-06-23 19:18:07 +0200

    Second migration: seeds users and addresses by model data seed

--- MyShop/Entities/Context.cs
+++ MyShop/Entities/Context.cs
@@ -49,7 +49,7 @@ namespace MyShop
                 //set primary key to UserId in class Address
                 .HasForeignKey<Address>(a => a.UserID);
             });
-
+            
             modelBuilder.Entity<Product>(eb =>
             {
                 eb.Property(p => p.Name).IsRequired();
@@ -66,13 +66,28 @@ namespace MyShop
                 .HasForeignKey(p => p.CategoryId);
             });
 
+            //MODEL DATA SEED
+            //the best use when we want put static data
+            //this is data which appear from beginning
+            modelBuilder.Entity<User>()
+                .HasData(new User { Id = 1, Name = "Marcin", LastName = "Kowalski", Email = "someemail1@gmail.com" },
+                new User { Id = 2, Name = "Anna", LastName = "Nowak", Email = "someemail2@gmail.com" },
+                new User { Id = 3, Name = "Tomasz", LastName = "Wybicki", Email = "someemail3@gmail.com" },
+                new User { Id = 4, Name = "Marek", LastName = "Nadgórski", Email = "someemail4@gmail.com" });
+
+            modelBuilder.Entity<Address>()
+                .HasData(new Address { Id = 1, Cntry = "Poland", City = "Katowice", Street = "Długa", PostalCode = "40-800", UserID = 1 },
+                new Address { Id = 2, Cntry = "Poland", City = "Chorzów", Street = "Krótka", PostalCode = "42-900", UserID = 2 },
+                new Address { Id = 3, Cntry = "Poland", City = "Siemianowice Śląskie", Street = "Szeroka", PostalCode = "43-900", UserID = 3 },
+                new Address { Id = 4, Cntry = "Poland", City = "Katowice", Street = "Wąska", PostalCode = "44-880", UserID = 4 });
+
+
             //modelBuilder.Entity<ProductUser>()
-                
-                //primary (compound) key
-                //.HasKey(pu => new { pu.UserId, pu.ProductId });
-                
-                
-                
+            //primary (compound) key
+            //.HasKey(pu => new { pu.UserId, pu.ProductId });
+
+
+
         }
     }
 }
---
+++ MyShop/Migrations/20220623171206_UserAddressSeeds.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyShop.Migrations
+{
+    public partial class UserAddressSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "LastName", "Name" },
+                values: new object[,]
+                {
+                    { 1, "someemail1@gmail.com", "Kowalski", "Marcin" },
+                    { 2, "someemail2@gmail.com", "Nowak", "Anna" },
+                    { 3, "someemail3@gmail.com", "Wybicki", "Tomasz" },
+                    { 4, "someemail4@gmail.com", "Nadgórski", "Marek" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Adresses",
+                columns: new[] { "Id", "City", "Country", "PostalCode", "Street", "UserID" },
+                values: new object[,]
+                {
+                    { 1, "Katowice", "Poland", "40-800", "Długa", 1 },
+                    { 2, "Chorzów", "Poland", "42-900", "Krótka", 2 },
+                    { 3, "Siemianowice Śląskie", "Poland", "43-900", "Szeroka", 3 },
+                    { 4, "Katowice", "Poland", "44-880", "Wąska", 4 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Adresses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Adresses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Adresses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Adresses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
